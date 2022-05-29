39489c0ca3b173d62bb31b0d4a409ff18cf7c6ec
rexon <rexonms@gmail.com>
2022-05-29 14:25:46 -0500

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,23 @@
-# Start from the latest golang base image
-FROM golang:latest
-
-# Add Maitainer Info
-LABEL maitainer="rexon <rexonms@gmail.com>"
-
-# Set the Current Working Directory inside the container
+# syntax=docker/dockerfile:1
+FROM golang:1.18-alpine
 WORKDIR /app
 
-# Copy Go Modules dependency requirements file
-COPY go.mod .
-
-# Copy Go Modules expected hashes file
-COPY go.sum .
-
-# Download dependencies 
+COPY go.mod ./
+COPY go.sum ./
 RUN go mod download
 
-# Copy all the app sources (recursively copies files and directories from the host into container folder)
-COPY . .
+COPY *.go ./
+RUN go build -o /rexonms/kalomaya
 
-# Set http port
-ENV PORT 8000
+EXPOSE 8080
 
-# Build the app
-RUN go build
+CMD [ "/rexonms/kalomaya" ]
 
 # Remove source files
 RUN find . -name "*.go" -type f -delete
 
 # Make port 8000 available to the world outside this container 
-EXPOSE ${PORT}
+EXPOSE 8080
 
 # Run the app
-CMD ["./kalomaya"]
+CMD ["./rexonms/kalomaya"]
--- server.go
+++ server.go
@@ -8,65 +8,20 @@ import (
 	"github.com/rexonms/go/greet"
 )
 
-const defaultPort = ":8000"
+const defaultPort = ":8080"
 
 func main() {
-	fmt.Println(greet.Hello())
-	// loggedUser := me {
-	// 	_id: "61975a77f25626d3530d733f", 
-	// 	email : "53863c83-f221-4a8f-b70c-73fa9a5a6932@guest.com", 
-	// 	sub : "53863c83-f221-4a8f-b70c-73fa9a5a6932", 
-	// 	subscriptionType : "G1", 
-	// 	downPayment : 25, 
-	// 	interestRate : 3.5, 
-	// 	loanYear : 30, 
-	// 	subscriptionHistory: nil, 
-	// 	calculatedPropertyList : nil, 
-	// 	favoritePropertyList : nil, 
-	// 	searchedQueryList : nil, 
-	// }
-	// fmt.Println(loggedUser)
-	// fmt.Println(loggedUser.getEmail())
-
-	// Print even or odd 
-	// myInts := []int{0,1,2,3,4,5,6,7,8,9,10}
-
-	// for _, myInt := range myInts{
-	// 	if myInt % 2 == 0 {
-	// 		fmt.Println(myInt,  " is even")
-	// 	}  else {
-	// 		fmt.Println(myInt,  " is odd")
-	// 	}
-	// }
-
-	// Structs 
-	// alex := person{firstName: "Alex", lastName: "Smith"}
-	// fmt.Println(alex)
-
-
-	// cards := newDeckFromFile("MyCards")
-	// cards.shuffle()
-	// // fmt.Println(cards.toString())
-	// // cards.saveToFile("MyCards")
-
-	// cards.print()
-	// hand, remainingDeck := deal(cards, 10)
-	// hand.print()
-	// remainingDeck.print()
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-
-	// greeting := "Hi there!"
-	// fmt.Println([]byte(greeting))
 	server := gin.Default()
 	//  server.GET("/", http.PlaygroundHandler())
 	//  server.POST("/query", http.GraphQLHandler())
 	server.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": greet.Hello(),
+			"foo": "bazz",
 		})
 	})
 	 err := server.Run(port)
