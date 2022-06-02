d30b1019d14efec2be341b2dd606afeb2a6858e3
Bikram Tuladhar <bikram.tuladhar@yipl.com.np>
2022-06-02 21:20:27 +0545

    react-docker-fixes
    * [x] added healthcheck in both react and golang docker
    * [x] created healthcheck function in golang to determine healthcheck of go code
    * [x] fixes react router issue with nginx with custom nginx config

---
+++ server/healthcheck.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Expected URL as command-line argument")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if (err != nil) || (resp.StatusCode != 200) {
+		os.Exit(1)
+	}
+}
